6c6058dd4a1507b659e59da2a2243b3b391d390d
Veranika <veranika.bazhanok@gmail.com>
2025-01-19 23:01:59 +0100

    feat: correct script solution 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,8 +4,38 @@ const path = require('path');
 const filesDir = path.join(__dirname, 'files');
 const filesCopy = path.join(__dirname, 'files-copy');
 
+async function clearDir(directory) {
+  const files = await fs.readdir(directory, { withFileTypes: true });
+
+  for (let file of files) {
+    const filePath = path.join(directory, file.name);
+    if (file.isFile()) {
+      await fs.unlink(filePath);
+    } else if (file.isDirectory()) {
+      await fs.rm(filePath, { recursive: true, force: true });
+    }
+  }
+}
+
+async function copyDirContent(source, destination) {
+  const files = await fs.readdir(source);
+
+  for (let file of files) {
+    const sourcePath = path.join(source, file);
+    const destinationPath = path.join(destination, file);
+    const stats = await fs.stat(sourcePath);
+
+    if (stats.isDirectory()) {
+      await fs.mkdir(destinationPath, { recursive: true });
+      await copyDirContent(sourcePath, destinationPath);
+    } else if (stats.isFile()) {
+      await fs.copyFile(sourcePath, destinationPath);
+    }
+  }
+}
 async function copyDir() {
-  await fs.mkdir(filesCopy, {recursive: true});
+  await fs.mkdir(filesCopy, { recursive: true });
+  await clearDir(filesCopy);
   const files = await fs.readdir(filesDir);
 
   for (let file of files) {
@@ -13,12 +43,14 @@ async function copyDir() {
     const copyPath = path.join(filesCopy, file);
     const stats = await fs.stat(filesPatch);
 
-    if (stats.isFile()) {
+    if (stats.isDirectory()) {
+      await fs.mkdir(copyPath);
+      await copyDirContent(filesPatch, copyPath);
+    } else if (stats.isFile()) {
       await fs.copyFile(filesPatch, copyPath);
     }
   }
   console.log('Copying is complete!');
 }
 
-copyDir()
-
+copyDir();
