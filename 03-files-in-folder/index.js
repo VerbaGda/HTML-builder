56ddd9c4352a69ecc1cc041bf61fd0c2f372e5bd
Veranika <veranika.bazhanok@gmail.com>
2025-01-19 02:16:11 +0100

    feat: change 03-files-in folder solution

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,14 +1,14 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
 
 const folderPath = path.join(__dirname, 'secret-folder');
 
-function getFileInfo() {
-  const files = fs.readdirSync(folderPath, 'utf8');
+async function getFileInfo() {
+  const files = await fs.readdir(folderPath);
 
   for (let file of files) {
     const filePath = path.join(folderPath, file);
-    const stats = fs.statSync(filePath);
+    const stats = await fs.stat(filePath);
 
     if (stats.isFile()) {
       const extname = path.extname(file).slice(1);
---
+++ 04-copy-directory/index.js
@@ -0,0 +1,22 @@
+// const fs = require('fs');
+// const path = require('path');
+
+// const folderPath = path.join(__dirname, 'files');
+
+// const files=fs.readdirSync(folderPath,'utf8');
+
+// fs.mkdir(path.join(__dirname, 'files-copy'))
+
+// // function makeDirectory() {
+// //   const projectFolder = join(
+// //     __dirname,
+// //     'test-css.css',
+// //     'test-image.jpg',
+// //     'test-js.js',
+// //     'test-text.txt',
+// //   );
+// //   const dirCreation=fs.mkdir(projectFolder);
+// //   return dirCreation
+// // }
+
+// // makeDirectory()
